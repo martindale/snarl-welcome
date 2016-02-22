9e7c0723c2b89f29f847e14282346bf72c01fc04
Eric Martindale <eric@ericmartindale.com>
2016-02-21 17:14:05 -0800

    Use new snarl plugin feature instead of emulating / waiting.

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 module.exports = {
-  '{USER}': function(user, cb) {
+  '{USER:NEW}': function(message, cb) {
     var self = this;
     if (!self.config || !self.config.welcome) return;
 
-    // simulate a real user delay
-    setTimeout(function() {
-      var knownUser = ~Object.keys(self.userMap).indexOf(user.id);
-      if (!knownUser) {
-        self.__rpc('im.open', {
-          user: user.id
-        }, function(err, response) {
-          if (!response || !response.channel || !response.channel.id) {
-            return console.error('Invalid response:', err, response);
-          }
+    var user = message.user;
 
-          if (self.config.welcome instanceof Array) {
-            var timing = 0;
-            var spacing = 15000;
-            self.config.welcome.forEach(function(message) {
-              setTimeout(function() {
-                self.__say(response.channel.id, formatText(message));
-              }, timing);
-              timing += spacing;
-            });
-          } else {
-            self.__say(response.channel.id, self.config.welcome);
-          }
-        });
-
+    self.__rpc('im.open', {
+      user: user.id
+    }, function(err, response) {
+      if (!response || !response.channel || !response.channel.id) {
+        return console.error('Invalid response:', err, response);
       }
-    }, 1000);
+
+      if (self.config.welcome instanceof Array) {
+        var timing = 15000; // start timing (send first message 15 seconds in)
+        var spacing = 15000; // interval between messages
+        self.config.welcome.forEach(function(message) {
+          setTimeout(function() {
+            self.__say(response.channel.id, formatText(message));
+          }, timing);
+          timing += spacing;
+        });
+      } else {
+        self.__say(response.channel.id, formatText(self.config.welcome));
+      }
+    });
 
     function formatText(input) {
       for (var id in self.channelMap) {
@@ -37,8 +32,8 @@ module.exports = {
         input = input.replace('{{channel:'+channel.name+'}}', '<#'+id+'>');
       }
       for (var id in self.userMap) {
-        var channel = self.userMap[id];
-        input = input.replace('{{channel:'+channel.name+'}}', '<#'+id+'>');
+        var user = self.userMap[id];
+        input = input.replace('{{user:'+user.name+'}}', '<#'+id+'>');
       }
       input = input.replace('{{user}}', '<@'+user.id+'>');
       return input;
